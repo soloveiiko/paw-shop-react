3e8544e97244fcd577a88d6ae6a84f951a631b5d
soloveiiko <soloveymaria51@gmail.com>
2023-09-15 16:58:44 +0300

    added: auth style

--- src/components/Modals/Auth/InputField.jsx
+++ src/components/Modals/Auth/InputField.jsx
@@ -1,10 +1,12 @@
-import { ErrorMessage, Field } from 'formik';
+import { ErrorMessage, Field, useFormikContext } from 'formik';
 
 const InputField = ({ className, name, placeholder }) => {
+  const { errors, touched } = useFormikContext(); // Отримуємо помилки та дотик поля з контексту Formik
+
   return (
     <>
       <Field className={`${className}__input`} placeholder={placeholder} name={name} />
-      <ErrorMessage className={`${className}__error`} component="div" name={name} />
+      {errors[name] && touched[name] && <ErrorMessage className={`${className}__error`} component="div" name={name} />}
     </>
   );
 };
