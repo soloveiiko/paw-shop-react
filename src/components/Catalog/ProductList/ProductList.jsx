115a4ee4b7b505a8368b1a379830adf635f02076
soloveiiko <soloveymaria51@gmail.com>
2023-09-18 17:47:49 +0300

    added: pagination

--- src/components/Catalog/ProductList/ProductList.jsx
+++ src/components/Catalog/ProductList/ProductList.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { popularProducts } from '@utils/data';
 import ProductItem from '@components/ProductItem/ProductItem';
+import ReactPaginate from 'react-paginate';
 
 const ProductList = () => {
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 2;
+
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = popularProducts.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(popularProducts.length / itemsPerPage);
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % popularProducts.length;
+    console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+    setItemOffset(newOffset);
+  };
   return (
     <div className="product-list">
       <div className="product-list__wrapper">
-        {popularProducts.map((product) => (
+        {currentItems.map((product) => (
           <ProductItem product={product} />
         ))}
       </div>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 };
